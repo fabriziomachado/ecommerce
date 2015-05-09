98fcb0009335932efb20b1756427c2695ac125a2
Fabrizio Machado <fabrizio.machado@gmail.com>
2015-05-09 20:27:56 -0300

    include routes for products and categories

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,12 +11,25 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::group(['namespace' => 'Admin'], function() {
 
+    Route::group(['prefix'=>'admin'], function(){
+        Route::get('categories', ['as'=>'categories.index', 'uses' => 'CategoriesController@index']);
+        Route::get('categories/create', ['as'=>'category.create', 'uses' => 'CategoriesController@create']);
+        Route::get('categories/{id}/edit', ['as'=>'category.edit', 'uses' => 'CategoriesController@edit']);
+        Route::get('categories/{id}/destroy', ['as'=>'category.destroy', 'uses' => 'CategoriesController@destroy']);
+
+        Route::get('products', ['as'=>'products.index', 'uses' => 'ProductsController@index']);
+        Route::get('products/create', ['as'=>'product.create', 'uses' => 'ProductsController@create']);
+        Route::get('products/{id}/edit', ['as'=>'product.edit', 'uses' => 'ProductsController@edit']);
+        Route::get('products/{id}/destroy', ['as'=>'product.destroy', 'uses' => 'ProductsController@destroy']);
+    });
+});
+
+
+Route::get('/', 'HomeController@index');
 Route::get('example', 'WelcomeController@example');
-
 Route::get('home', 'HomeController@index');
-
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
