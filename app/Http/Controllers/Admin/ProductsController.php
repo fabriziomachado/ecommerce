4a8cc0730536ac5c7c635436807ad7950d306acc
Fabrizio Machado <fabrizio.machado@gmail.com>
2015-05-13 18:43:29 -0300

    ajustando rotas para o crud

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -15,11 +15,6 @@ class ProductsController extends Controller {
         $this->productModel = $productModel;
     }
 
-    /**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
 	public function index()
 	{
         $products = $this->productModel->all();
@@ -27,65 +22,31 @@ class ProductsController extends Controller {
         return view('product.index', compact('products'));
 	}
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
 	public function create()
 	{
 		//
 	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
 	public function store()
 	{
 		//
 	}
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function show($id)
 	{
 		//
 	}
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function edit($id)
 	{
 		//
 	}
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function update($id)
 	{
 		//
 	}
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function destroy($id)
 	{
 		//
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,17 +13,30 @@
 
 Route::group(['namespace' => 'Admin'], function() {
 
-    Route::group(['prefix'=>'admin'], function(){
-        Route::get('categories', ['as'=>'categories.index', 'uses' => 'CategoriesController@index']);
-        Route::get('categories/create', ['as'=>'category.create', 'uses' => 'CategoriesController@create']);
-        Route::get('categories/{id}/edit', ['as'=>'category.edit', 'uses' => 'CategoriesController@edit']);
-        Route::get('categories/{id}/destroy', ['as'=>'category.destroy', 'uses' => 'CategoriesController@destroy']);
+    # Admin
+    Route::group(['prefix'=>'admin', 'where'=>['id'=>'[0-9]+']], function(){
 
-        Route::get('products', ['as'=>'products.index', 'uses' => 'ProductsController@index']);
-        Route::get('products/create', ['as'=>'product.create', 'uses' => 'ProductsController@create']);
-        Route::get('products/{id}/edit', ['as'=>'product.edit', 'uses' => 'ProductsController@edit']);
-        Route::get('products/{id}/destroy', ['as'=>'product.destroy', 'uses' => 'ProductsController@destroy']);
+        # Categories
+        Route::group(['prefix'=>'categories'], function(){
+            Route::get('/', ['as'=>'categories.index', 'uses' => 'CategoriesController@index']);
+            Route::get('create', ['as'=>'category.create', 'uses' => 'CategoriesController@create']);
+            Route::post('/', ['as'=>'category.store', 'uses'=> 'CategoriesController@store']);
+            Route::get('{id}/destroy', ['as'=>'category.destroy', 'uses' => 'CategoriesController@destroy']);
+            Route::get('{id}/edit', ['as'=>'category.edit', 'uses' => 'CategoriesController@edit']);
+            Route::put('{id}/update', ['as'=>'category.update', 'uses'=>'CategoriesController@update']);
+        });
+        # Products
+        Route::group(['prefix'=>'products'], function(){
+            Route::get('/', ['as'=>'products.index', 'uses' => 'ProductsController@index']);
+            Route::get('create', ['as'=>'product.create', 'uses' => 'ProductsController@create']);
+            Route::post('/', ['as'=>'product.store', 'uses'=> 'ProductsController@store']);
+            Route::get('{id}/destroy', ['as'=>'product.destroy', 'uses' => 'ProductsController@destroy']);
+            Route::get('{id}/edit', ['as'=>'product.edit', 'uses' => 'ProductsController@edit']);
+            Route::put('{id}/update', ['as'=>'product.update', 'uses'=>'ProductsController@update']);
+        });
+
     });
+
 });
 
 
