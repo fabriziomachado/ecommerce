5ea3795c543d86ba2c3fcd417b0c97b42766eed3
vagrant <vagrant@homestead>
2015-05-27 18:26:38 +0000

    Criando Model

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -9,5 +9,5 @@
     {!! Form::model($product, ['route'=>'products.store', 'method'=>'post']) !!}
         @include('products._form', ['submit_button_text' => trans('app.create_product')])
     {!! Form::close() !!}
-    
+
 @endsection
