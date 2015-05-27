13e9f11e08e745d533ac332741b1bfdcc0561e5d
vagrant <vagrant@homestead>
2015-05-27 19:01:59 +0000

    Configurando controller e rotas e Listando imagens

---
+++ resources/views/products/images.blade.php
@@ -0,0 +1,39 @@
+@extends('app')
+
+@section('content')
+
+    <div class="container">
+        <h1>Images of {{ $product->name }}</h1>
+
+        <a href="{{ route('products.images.create', ['id'=>$product->id]) }}" class="btn btn-primary">New Image</a>
+        <br/>
+        <br/>
+
+        <table class="table">
+            <tr>
+                <th>ID</th>
+                <th>Image</th>
+                <th>Extension</th>
+                <th>Action</th>
+            </tr>
+
+            @foreach($product->images as $image)
+                <tr>
+                    <td>{{ $image->id  }}</td>
+                    <td>
+                        {{ asset('uploads/'.$image->id.'.'.$image->extension, 'product image', array('width' => '80px')) }}
+                    </td>
+                    <td>{{ $image->extension }}</td>
+                    <td>
+                        <a href="{{ route('products.images.destroy',['id'=>$image->id]) }}" class="btn btn-xs btn-danger">Delete</a>
+                    </td>
+                </tr>
+            @endforeach
+
+        </table>
+
+        <a href="{{ route('products.index') }}" class="btn btn-default">Voltar</a>
+
+    </div>
+
+@endsection
