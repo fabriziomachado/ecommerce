20a6a894d2c6d063e435371185409e288f0f2da5
Fabrizio Machado <fabrizio.machado@gmail.com>
2015-07-04 11:36:50 -0300

    pagseguro checkout

---
+++ config/pagseguro.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'environment' => 'sandbox', // production
+    'email' => env('PAGSEGURO_MAIL', 'dev@pagseguro.com'),
+    'token' => env('PAGSEGURO_TOKEN', 'token-id')
+];
+
+
---
+++ database/migrations/2015_07_04_132858_add_pagseguro_trans_id_to_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPagseguroTransIdToOrdersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::table('orders', function(Blueprint $table)
+        {
+            $table->string('pagseguro_trans_id')->nullable();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::table('orders', function(Blueprint $table)
+        {
+            $table->removeColumn('pagseguro_trans_id');
+        });
+	}
+
+}
